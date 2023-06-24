b18a0f02335b6bfce7950cc3c9ba92258e7bf6d5
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2023-06-24 23:01:50 +0700

    1st version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import reactlogo from './assets/images/react-icon.png'
+import './assets/styles/app.css'
+import './assets/styles/app.scss'
+
+const App = () => {
+  return (
+    <div>
+      <img src={reactlogo} alt='React Logo' width={100} height={100} />
+      <h1>ABC</h1>
+      <h2>DEF</h2>
+    </div>
+  )
+}
+
+export default App
